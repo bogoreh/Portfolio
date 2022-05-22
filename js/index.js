6e962ab86dbf603e7c786f40407aeac13cc96a1b
bogoreh <bogoreh026@gmail.com>
2022-05-22 23:21:55 +0300

    The first commit

--- js/index.js
+++ js/index.js
@@ -1,31 +1,3 @@
-/* 
-const showMenu = (toggleId, navId) => {
-
-    const toggle = document.getElementById(toggleId),
-        nav = document.getElementById(navId)
-
-    if (toggle & nav) {
-        toggle.addEventListener('click', () => {
-            nav.classList.toggle('show')
-        })
-    }
-}
-showMenu('nav-toggle', 'nav-menu')
-
-
-const navLink = document.querySelectorAll('.nav__link')
-
-function linkAction() {
-    //Active link
-    navLink.forEach(n => n.classList.remove('active'))
-    this.classList.add('active')
-
-    // remove menu mobile
-    const navMenu = document.getElementById('nav-menu')
-}
-
-navLink.forEach(n => n.addEventListener('click', )) */
-
 /*===== MENU SHOW =====*/
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
